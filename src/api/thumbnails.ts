f607103d7effa35f755515ddd974a4474dbbf74c
Code0Light1 <kirankk105@gmail.com>
2025-07-04 09:49:28 +0100

    Upload thumbnail will get the thumbnil data from form data and save it to assets folder in File and serve it from there

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,15 +1,16 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "path";
 
 type Thumbnail = {
   data: ArrayBuffer;
   mediaType: string;
 };
-
+const MAX_UPLOAD_SIZE = 10 << 20; //10mb in bytes
 const videoThumbnails: Map<string, Thumbnail> = new Map();
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
@@ -41,13 +42,47 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   if (!videoId) {
     throw new BadRequestError("Invalid video ID");
   }
-
+  const mimeType = req.headers.get('content-type')
+  if (mimeType !== "image/jpeg" && mimeType !== "image/png" ) {
+    throw new BadRequestError('Unsupported file')
+  }
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = req.formData()
+  const imageData = (await formData).get('thumbnail')
+  if (!(imageData instanceof File)) {
+      throw new BadRequestError('Image data is invalid')
+  }
+  if (imageData.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError('Check your file size')
+  }
+  const mediaType = imageData.type
+  const arrayBuffer = await imageData.arrayBuffer()
+  const imageExtension = getExtensionFromMime(mediaType)
+  const assetPath = path.join(cfg.assetsRoot, `${videoId}.${imageExtension}`)
+  await Bun.write(assetPath, arrayBuffer);
 
-  return respondWithJSON(200, null);
+  const videoMetadata = await getVideo(cfg.db, videoId)
+  if(videoMetadata?.userID !== userID) {
+    throw new UserForbiddenError('Access denied')
+  }
+  const url = new URL(req.url)
+  const baseUrl = `${url.protocol}/${url.host}`
+  const thumbnailUrl = path.join(baseUrl, assetPath)
+  videoMetadata.thumbnailURL = thumbnailUrl
+  await updateVideo(cfg.db, videoMetadata)
+  return respondWithJSON(200, videoMetadata);
+}
+
+function getExtensionFromMime(mime: string) {
+  const map: Record<string, string> = {
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+    'image/gif': 'gif',
+    // Add more as needed
+  };
+  return map[mime] || 'bin'; // fallback to .bin for unknown types
 }
